b6b8d7feb6fe6ac59720e9619e4228c42e2108c5
Vitor Hugo Kokkonen <117650625+VitorKokkonen@users.noreply.github.com>
2023-07-20 17:53:14 -0300

    Inserindo email

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const form = document.querySelector('form')
 const card = document.querySelector('.card')
 const btnDismiss = document.querySelector('.button-scs')
 const sucesso = document.querySelector('.sucessed')
-
+const span = document.getElementById('span')
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
     removeCard()
     const emailInput = form.querySelector('input[type="email"]')
-    console.log(emailInput.value)
+    span.textContent = emailInput.value
     
 })
 
