a282e2432e31a4cca83a17bec6a6eb2438887b3f
Vitor Hugo Kokkonen <117650625+VitorKokkonen@users.noreply.github.com>
2023-07-27 10:21:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,32 +3,47 @@ const card = document.querySelector('.card')
 const btnDismiss = document.querySelector('.button-scs')
 const sucesso = document.querySelector('.sucessed')
 const span = document.getElementById('span')
+const input = form.querySelector('input[type="email"]')
 
-form.addEventListener('submit', (e) =>{
+form.addEventListener('submit', (e) => {
     e.preventDefault()
-
+    validateEmail()
     const emailInput = form.querySelector('input[type="email"]')
     const emailCapturado = emailInput.value
+    span.textContent = emailCapturado
     
-    if (emailCapturado == '') {
-        alert('Preencha um Email');
-    } else {
-        span.textContent = emailCapturado
-
-        removeCard()
-    }
+    
 })
+
 
 btnDismiss.addEventListener('click', () => {
     removeSucesso()
 })
 
-function removeCard(){
+function removeCard() {
     card.style.display = 'none'
     sucesso.style.display = 'flex'
 }
 
-function removeSucesso(){
+function removeSucesso() {
     card.style.display = 'flex'
     sucesso.style.display = 'none'
+}
+
+function validateEmail () {
+    const email = document.getElementById("email").value;
+    const errorMensage = document.querySelector(".error")
+    let invalidEmail = /\S+@\S+\.\S+/;
+    
+    
+    if(!invalidEmail.test(email)){
+        errorMensage.style.display = "block"
+        input.style.backgroundColor = "#f6adad"
+
+    }else {
+        removeCard()
+        errorMensage.style.display = "none"
+        input.style.backgroundColor = "white"
+    }
+    
 }
